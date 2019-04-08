9af8a951a8b88f0e96689305e5f98f6ab7049a2c
shaokaiyang <kaiyang2013@outlook.com>
2019-04-08 23:33:44 +0800

    callable&threadpool

---
+++ src/Multithreading/CallableTest.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+import java.util.concurrent.*;
+
+import static java.util.Arrays.asList;
+
+public class CallableTest {
+
+    static class Sum implements Callable<Long> {
+        private final long from;
+        private final long to;
+
+        Sum(long from, long to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public Long call() {
+            long acc = 0;
+            for (long i = from; i <= to; i++) {
+                acc = acc + i;
+            }
+            System.out.println(Thread.currentThread().getName() + " : " + acc);
+            return acc;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<Future<Long>> results = executor.invokeAll(asList(
+                new Sum(0, 10), new Sum(0, 1_000), new Sum(0, 1_000_000)
+        ));
+        executor.shutdown();
+
+        for (Future<Long> result : results) {
+            System.out.println(result.get());
+        }
+    }
+}
